cc4854c188d5aaf0b27a68ec69159c0a074339a8
James Zhang <jameszhanga@gmail.com>
2021-05-24 17:46:45 -0500

    script up and running

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import shutil
 from pathlib import Path
 
-# experiment_dir = Path.joinpath(Path.cwd(), 'Experiment')
-# print(experiment_dir)
-
-# file1 = Path.joinpath(experiment_dir, '1', 'file1.txt')
-
 src = Path('G:', '/', '_PROJECT FILES', "_Old PROJ NO's", 'CLOSED PROJECTS','COMPLETED PROJECTS', '1000-Bedliners','_Non-Numbered Projects')
-dst = Path('G:\DE\Test Results Archive\XXXX-2021')
+dst = Path('G:\DE\Test Results Archive\XXXX-2021\Tests')
 
-# shutil.move(file1, Path.joinpath(experiment_dir,'2'))
-# shutil.move(dst, file1)
+# print(src)
+# print(src.is_dir())
 
-print(src)
-print(src.is_dir())
+# print(dst)
+# print(dst.is_dir())
 
-print(dst)
-print(dst.is_dir())
+for dir in src.iterdir():
+    if dir.is_dir:
+        for sub in dir.iterdir():
+            path_string = str(Path(sub))
+            if ('test' or 'testing') in (path_string.casefold()):
+                if sub.is_dir():
+                    for files in sub.iterdir():
+                        if not files.is_dir() and ('pdf' in str(Path(files)).casefold()):
+                            print(files)
+                            shutil.copy2(str(Path(files)), dst)
+                elif not sub.is_dir() and ('pdf' in path_string):
+                    shutil.copy2(path_string, dst)
