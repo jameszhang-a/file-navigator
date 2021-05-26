86f057e59a1965cd2f7889ad48429e646a99269e
James Zhang <jameszhanga@gmail.com>
2021-05-26 16:44:43 -0500

    new algo implemented, still need to fix file not found exception in some cases

--- main.py
+++ main.py
@@ -23,23 +23,48 @@ dst = Path(
 
 
 # Copies only files from src to dst
-def copy_file(src, dst=None):
-    for (root, dir, files) in os.walk(src):  # recursively traverses to each file
-        for y in files:
-            temp = y.casefold()  # to perserve the original file ending
-            if (".pdf" in temp or ".xlsx" in temp) and (
-                "quote" not in temp
-                and "po " not in temp
-                and "quotation" not in temp
-                and "qu-" not in temp
-                and "msg" not in temp
-            ):
-                file_path = Path(root, y)
-                try:
-                    shutil.copy2(file_path, dst)
-                except:
-                    unadded.append(f"PROJECT: {project} \nFILE: {y} \nPATH: {file_path}\n\n")
-                f.writelines([y, "\n"])
+# def copy_file(src, dst=None):
+#     for (root, dir, files) in os.walk(src):  # recursively traverses to each file
+#         for y in files:
+#             temp = y.casefold()  # to perserve the original file ending
+#             if (".pdf" in temp or ".xlsx" in temp) and (
+#                 "quote" not in temp
+#                 and "po " not in temp
+#                 and "quotation" not in temp
+#                 and "qu-" not in temp
+#                 and "msg" not in temp
+#             ):
+#                 file_path = Path(root, y)
+#                 try:
+#                     shutil.copy2(file_path, dst)
+#                 except:
+#                     unadded.append(
+#                         f"PROJECT: {project} \nFILE: {y} \nPATH: {file_path}\n\n"
+#                     )
+#                 f.writelines([y, "\n"])
+
+
+def copy_condition(filepath):
+    return True
+
+
+def recur_copy(src, dst):
+    """Recursively copies files from source directory to destination
+
+
+    Args:
+        src (Path): Path to orgin directory
+        dst (Path): Path to desired location
+    """
+    for item in os.listdir(src):
+        filepath = Path(src, item)
+
+        if os.path.isfile(filepath) and copy_condition(filepath):
+            shutil.copy2(filepath, dst)
+
+        elif os.path.isdir(filepath):
+            recur_copy(filepath, dst)
+
 
 # Logs the process
 f = open("log.txt", "w")
@@ -61,22 +86,24 @@ for project in os.listdir(src):
             "\n",
         ]
     )
-    for (root, dir, files) in os.walk(Path(src, project)):
-        # only looks at ones that has more directories to avoid double inclusion
-        if dir:
-            for test_dir in dir:
-                if ("test" or "testing") in test_dir.casefold() and not (
-                    "hot" or "po" or "box"
-                ) in test_dir.casefold():
-                    # test_dir is now a directory that contain tests
-                    org = Path(root, test_dir)
-                    copy_file(org, dst)
+    recur_copy(project, dst)
+    
+    # for (root, dir, files) in os.walk(Path(src, project)):
+    #     # only looks at ones that has more directories to avoid double inclusion
+    #     if dir:
+    #         for test_dir in dir:
+    #             if ("test" or "testing") in test_dir.casefold() and not (
+    #                 "hot" or "po" or "box"
+    #             ) in test_dir.casefold():
+    #                 # test_dir is now a directory that contain tests
+    #                 org = Path(root, test_dir)
+    #                 copy_file(org, dst)
 
 f.write(
-        "\n-------------------------------------------UNADDED-------------------------------------------\n"
-    )
+    "\n-------------------------------------------UNADDED-------------------------------------------\n"
+)
 for remaining in unadded:
     f.write(remaining)
 
-print(f'There are {len(unadded)} files not added')
+print(f"There are {len(unadded)} files not added")
 f.close()
